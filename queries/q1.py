67d58910ccbac4c7ae03dcb296b421ec61dcab39
DimitrisAndreadis <61821098+DimitrisAndreadis@users.noreply.github.com>
2023-02-04 15:10:52 +0200

    Update q1.py

--- queries/q1.py
+++ queries/q1.py
@@ -1,9 +1,4 @@
-def q1(spark, df):
-    print("hi 1")
-    df.createOrUpdateTempView("taxis")
-
-    # id of trip which element ????
-    df.select("VendorID").filter(df["DOLocationID"] == "Battery Park").max("Tip_amount").show()
-    sql = spark.sql("SELECT VendorID FROM taxis WHERE DOLocationID == 'Battery Park' ")
-    sql.show()
+def q1(spark, df1, df2):
+        df = df1.join(df2, df1.DOLocationID == df2.LocationID , "inner").select(df1.tpep_pickup_datetime.alias("datetime"), "Zone", "tip_amount")
+        df.filter((df["Zone"] == "Battery Park") & (df["datetime"].like("%-03-%"))).groupBy().max("tip_amount").show()
     
