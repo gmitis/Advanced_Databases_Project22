651348fba1ec6f72d3390e9ff731162af22e328a
DimitrisAndreadis <61821098+DimitrisAndreadis@users.noreply.github.com>
2023-02-04 15:12:47 +0200

    Update q2.py

--- queries/q2.py
+++ queries/q2.py
@@ -1,1 +1,2 @@
-print("hi 2")
+def q2(spark, df):    
+      df.filter(month("tpep_pickup_datetime")<7).groupBy(month("tpep_pickup_datetime").alias("month")).max("tolls_amount").sort("month").show()
