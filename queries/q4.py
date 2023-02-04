d7e5bd4325447ad95b18ab69bd7ba9562a4e5bfe
DimitrisAndreadis <61821098+DimitrisAndreadis@users.noreply.github.com>
2023-02-04 15:18:46 +0200

    Update q4.py

--- queries/q4.py
+++ queries/q4.py
@@ -1,1 +1,11 @@
-print("hi 4")
+from pyspark.sql.functions import hour,dayofweek, col, row_number
+from pyspark.sql.window import Window
+
+def q4(spark, df):
+        w2 = Window.partitionBy("weekday").orderBy(col("avg(passenger_count)").desc())
+        df.groupBy(hour("tpep_pickup_datetime").alias("hour"), dayofweek("tpep_pickup_datetime").alias("weekday"))\
+          .avg("passenger_count")\
+          .withColumn("row",row_number().over(w2))\
+          .filter(col("row") < 4)\
+          .orderBy("weekday","row")\
+          .drop("row").show(21)
