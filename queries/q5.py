5c06c1d81ca55713bc941a3ac982ed7387130170
DimitrisAndreadis <61821098+DimitrisAndreadis@users.noreply.github.com>
2023-02-04 15:19:49 +0200

    Update q5.py

--- queries/q5.py
+++ queries/q5.py
@@ -1,1 +1,14 @@
-print("hi 5")
+from pyspark.sql.functions import dayofmonth, dayofyear, month, col, row_number
+from pyspark.sql.window import Window
+
+def q5(spark, df):
+        w2 = Window.partitionBy("month").orderBy(col("avg(ratio)").desc())
+        df.filter(col("fare_amount")>0)\
+          .withColumn("ratio", col("tip_amount")/ col("fare_amount"))\
+          .groupBy(dayofyear("tpep_pickup_datetime").alias("yearday"), month("tpep_pickup_datetime").alias("month"))\
+          .avg("ratio")\
+          .withColumn("row",row_number().over(w2))\
+          .filter(col("row") < 6)\
+          .orderBy("month","row")\
+          .drop("row")\
+          .show(30)
